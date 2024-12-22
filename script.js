386f3d7c844151ac03a594f190333fb26f27ac33
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2024-12-22 13:00:39 -0500

    Added drawing to the etch
    Added hover function where the mouse "paints" the canvas when hovering over a cell

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-let rows = 16
-let cols = 16
-let pixelSize = 40
+let rows = 10
+let cols = 10
+let cellSize = 6
+let drawingSpaceSize = 600
 
 
 const gridContainer = document.querySelector(".gridContainer")
-gridContainer.style.height = `${cols * pixelSize}px`
-gridContainer.style.width = `${rows * pixelSize}px`
+gridContainer.style.height = `${drawingSpaceSize}px`
+gridContainer.style.width = `${drawingSpaceSize}px`
 
 for (let i=0; i < cols; i++) {
    for (let j = 0; j < rows; j++) {
     const cell = document.createElement("div")
+
+    //setting up cell style
+    cell.className = "cell"
     cell.style.backgroundColor = "white"
-    cell.style.padding = `${pixelSize / 2}px`
+    cell.style.padding = `${(drawingSpaceSize / rows) / 2}px`
     cell.style.textAlign = "center"
     gridContainer.appendChild(cell)
    }  
 }
+
+function onmouseHover(e) {
+   e.target.style.backgroundColor = "black"
+}
+
+cells = document.querySelectorAll(".cell")
+cells.forEach((cell) => {
+   cell.addEventListener("mouseover", onmouseHover)
+})
