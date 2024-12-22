9c0d080324b25a39dba066916e59b3cf8317aef3
kyeung36220 <135488116+kyeung36220@users.noreply.github.com>
2024-12-22 13:21:18 -0500

    Added functional edit resolution button

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
-let rows = 10
-let cols = 10
-let cellSize = 6
+let rowsInitial = 20
+let colsInitial = 20
 let drawingSpaceSize = 600
 
-
+//initialization
 const gridContainer = document.querySelector(".gridContainer")
 gridContainer.style.height = `${drawingSpaceSize}px`
 gridContainer.style.width = `${drawingSpaceSize}px`
+cellMaker(rowsInitial, colsInitial)
 
-for (let i=0; i < cols; i++) {
-   for (let j = 0; j < rows; j++) {
-    const cell = document.createElement("div")
+//making the functional board
+function cellMaker(rows, cols) {
 
-    //setting up cell style
-    cell.className = "cell"
-    cell.style.backgroundColor = "white"
-    cell.style.padding = `${(drawingSpaceSize / rows) / 2}px`
-    cell.style.textAlign = "center"
-    gridContainer.appendChild(cell)
-   }  
+   gridContainer.innerHTML = ""
+
+   for (let i=0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+       const cell = document.createElement("div")
+   
+       //setting up cell style
+       cell.className = "cell"
+       cell.style.backgroundColor = "white"
+       cell.style.padding = `${(drawingSpaceSize / rows) / 2}px`
+       cell.style.textAlign = "center"
+       gridContainer.appendChild(cell)
+      }  
+   }
+
+   function onmouseHover(e) {
+      e.target.style.backgroundColor = "black"
+   }
+   
+   cells = document.querySelectorAll(".cell")
+   cells.forEach((cell) => {
+      cell.addEventListener("mouseover", onmouseHover)
+   })
 }
 
-function onmouseHover(e) {
-   e.target.style.backgroundColor = "black"
+//editSize Button
+function editSize() {
+   while (true) {
+      size = prompt("What resolution? (Max 100)")
+      if (size > 0 && size < 100) {
+         break
+      }
+   }
+   cellMaker(size, size) // since same for row and col
 }
-
-cells = document.querySelectorAll(".cell")
-cells.forEach((cell) => {
-   cell.addEventListener("mouseover", onmouseHover)
-})
+const sizeButton = document.querySelector(".sizeButton")
+sizeButton.addEventListener("click", editSize)
